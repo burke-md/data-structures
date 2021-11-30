8e320f4a8f230efe00e58615733e811db6428d9f
Michael <michael.dburke56@gmail.com>
2021-11-30 15:11:03 -0500

    Initial commit w/ steps to solve problem.

---
+++ invertBinaryTree.js
@@ -0,0 +1,10 @@
+/*Invert Binary Tree:
+Return a mirror image of the input
+*/ 
+
+//Steps for iterative approach
+//-Create queue. Initial queue w/ root node.
+//-Remove node from queue(dequeue).
+//-Use helper function to swap child nodes.
+//-Add child nodes to queue.
+//-Continue until the queue is empty
