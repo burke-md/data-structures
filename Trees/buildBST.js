756258db0864084ad91634acc405eb978c601e1d
Michael <michael.dburke56@gmail.com>
2022-01-01 18:44:55 -0500

    Initial commit.

---
+++ Trees/buildBST.js
@@ -0,0 +1,16 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+  insert(value){
+
+  }
+  contains(value){
+
+  }
+  remove(value){
+    
+  }
+}
