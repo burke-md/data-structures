cba099eaf640e58d4a7ac4cbc9c02bc7c370d669
MB <michael.dburke56@gmail.com>
2022-05-30 19:49:47 -0500

    Define functions for solving airport problem.

--- Graphs/airportGraph.go
+++ Graphs/airportGraph.go
@@ -16,7 +16,62 @@ type AirportNode struct {
 	UnreachableConnection []string
 }
 
-func AirportConnections(airports []string, routes [][]string, startingAirport string) int {
+func NewAirportNode(airport string)*AirportNode {
+	return &AirportNode{
+		Airport: airport,
+		Connections: []string{},
+		IsReachable: true,
+		UnreachableConnection: []string{},
+	}
+}
+
+func AirportConnections(
+	airports []string, 
+	routes [][]string, 
+	startingAirport string) 
+	int {
 	
-	return -1
+}
+
+func createAirportGraph(
+	airports[]string, 
+	routes[][]string) 
+	map[string]*AirportNode {
+
+}
+
+func getUnreachableAirportNodes(
+	airportGraph map[string]*AirportNode, 
+	airports []string, 
+	strartingAirport string)
+	[]*AirportNode {
+
+}
+
+func deptFirstTraverseAirports(
+	airportGraph map[string]*AirportNode,
+	airport string,
+	visitedAirports map[string]bool) {
+
+}
+
+func markUnreachableConnections(
+	airportGraph map[string]*AirportNode,
+	unreachableAirportNodes []*AirportNode) {
+
+}
+
+func depthFirstAddUnreachableConnections(
+	airportGraph map[string]*AirportNode,
+	airport string,
+	unreachableConnections *[]string,
+	visitedAirports map[string]bool) {
+
+}
+
+func getMinNumberOfNewConnections(
+	airportGraph map[string] *AirportNode,
+	unreachableAirportNodes []*AirportNode)
+	int {
+
 }
